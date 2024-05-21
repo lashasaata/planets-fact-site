321949087ec9f2617c3dd5130b30980d19128750
lashasaata <lashasaata@gmail.com>
2024-05-21 06:09:27 -0700

    style: mobile style for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,24 @@ import "./App.css";
 import data from "../data.json";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
+import styled from "styled-components";
 
 function App() {
   const [useData, setUseData] = useState(data);
   const [burgerMenu, setBurgerMenu] = useState<boolean>(true);
   return (
-    <>
+    <MainCard>
       <Header />
       {burgerMenu ? <Menu useData={useData} /> : ""}
-    </>
+    </MainCard>
   );
 }
 
+const MainCard = styled.main`
+  min-height: min-content;
+  background-color: #070724;
+  background-image: url("/assets/background-stars.svg");
+  background-repeat: no-repeat;
+  padding-bottom: 1px;
+`;
 export default App;
