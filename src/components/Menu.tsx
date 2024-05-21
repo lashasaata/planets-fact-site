f2567783e1a92db18e3d2307cdcbb84249ba4df5
lashasaata <lashasaata@gmail.com>
2024-05-21 06:10:37 -0700

    style: add menu component mobile styles

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,21 +1,59 @@
 import React from "react";
+import styled from "styled-components";
 
 function Menu(props) {
+  const colors: { [key: string]: string } = {
+    Mercury: "#def4fc",
+    Venus: "#f7cc7f",
+    Earth: "#545bfe",
+    Mars: "#ff6a45",
+    Jupiter: "#ecad7a",
+    Saturn: "#fccb6b",
+    Uranus: "#65f0d5",
+    Neptune: "#497efa",
+  };
   return (
-    <main>
-      {props.useData.map((e) => {
+    <Main>
+      {props.useData.map((e, index: number) => {
+        const planetColor = colors[e.name];
+        console.log(planetColor);
         return (
-          <section>
-            <div>
-              <div></div>
-              <h2>{e.name}</h2>
+          <Section key={index} index={index}>
+            <div className="flex items-center gap-[25px]">
+              <div
+                className={`bg-[${planetColor}]  w-5 h-5 rounded-full`}
+              ></div>
+              <PlanetNames>{e.name}</PlanetNames>
             </div>
             <img src="./assets/icon-chevron.svg" alt="arrow" />
-          </section>
+          </Section>
         );
       })}
-    </main>
+    </Main>
   );
 }
-
+const Main = styled.main`
+  width: auto;
+  margin: 44px 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+const Section = styled.section<{ index: number }>`
+  width: 327px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom: ${(props) =>
+    props.index == 7 ? "" : "1px solid rgba(255, 255, 255, 0.1)"};
+`;
+const PlanetNames = styled.h2`
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 1.67;
+  letter-spacing: 1.36px;
+  color: white;
+`;
 export default Menu;
