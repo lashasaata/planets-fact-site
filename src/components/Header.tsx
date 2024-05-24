ee57dd2cfbd955a6702f0bf8618e58109c8a4753
lashasaata <lashasaata@gmail.com>
2024-05-24 13:58:40 -0700

    style: add header styles for tablet version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,22 +4,51 @@ import styled from "styled-components";
 function Header({
   burgerMenu,
   setBurgerMenu,
+  useData,
+  choosenPlanet,
+  setChoosenPlanet,
 }: {
   burgerMenu: boolean;
   setBurgerMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  useData: TPlanetInfo[];
+  choosenPlanet: string;
+  setChoosenPlanet: React.Dispatch<React.SetStateAction<string>>;
 }) {
   const handleMenu = () => {
     setBurgerMenu(!burgerMenu);
   };
 
+  const choosePlanet = (e) => {
+    const lowercase = e.target.innerText.toLowerCase().split("");
+    const firstUpper = lowercase[0].toUpperCase();
+    lowercase[0] = firstUpper;
+    setChoosenPlanet(lowercase.join(""));
+  };
+
   return (
-    <Heading>
+    <Heading className="md:flex-col md:items-center md:gap-10 py-4 md:pt-[32px] px-6 md:px-[52px] md:pb-[27px]">
       <MainText>THE PLANETS</MainText>
       <MenuIng
         onClick={handleMenu}
+        className="md:hidden"
         src="/assets/icon-hamburger.svg"
         alt="menu"
       />
+      <div className="hidden md:flex gap-10">
+        {useData.map((e, index: number) => {
+          return (
+            <h2
+              key={index}
+              onClick={choosePlanet}
+              className={`${
+                choosenPlanet == e.name ? "opacity-100" : "opacity-75"
+              } text-xs text-[#fff] font-[700] leading-[2.27] tracking-[1.5px]`}
+            >
+              {e.name.toUpperCase()}
+            </h2>
+          );
+        })}
+      </div>
     </Heading>
   );
 }
@@ -29,7 +58,6 @@ const Heading = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 16px 24px;
   border-bottom: 1px solid rgba(255, 255, 255, 0.2);
 `;
 
